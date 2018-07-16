6bcc6acf8cf7a77a1be350e02c77f27f74d23073
Nathan Chung <nchung111@gmail.com>
2018-07-15 22:37:07 -0400

    Add tests and renamed tests.py to test_figure.py

--- test_figure.py
+++ test_figure.py
@@ -1,5 +1,16 @@
 from geomancy import Geofigure, Geoshield
 
+def test_easy_figure():
+    seed = [1,2,1,2]
+    a = Geofigure(*seed)
+    assert a.nums == seed
+
+def test_hard_figure():
+    seed = [0,3,6,2]
+    answer = [2,1,2,2]
+    a = Geofigure(*seed)
+    assert a.nums == answer
+
 def run_test():
     new_vals = [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1]
     moms = []
