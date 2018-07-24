5953ba3544f9df3169ab8c71bf618f140aa94fd9
Nathan Chung <nchung111@gmail.com>
2018-07-24 03:16:49 -0400

    Get the GUI in geomancy/gui.py to work at a basic level. Add run.py at the top folder to run geomancy/gui.py.

--- geomancy/gui.py
+++ geomancy/gui.py
@@ -1,73 +1,83 @@
 import tkinter as tk
-from structures import Shield
+from .structures import Shield, Figure
+from .stringfuncs import center_lines
 
 class GeomancyApplication(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.grid()
+        self.master = master
+        self.default_font = 'Courier'
+        self.bgcolors = ('red','yellow','blue','green')
+        self.cur_throw = 0
+        self.figures = [None for m in range(Shield.num_mothers)]
+
+        sample_figure = Figure.quick_throw()
+        num_lines = len(str(sample_figure).split('\n'))
+        label_width = len(Figure.longest_name)
+        self.blank_lines = center_lines('\n' * ( num_lines-1), label_width)
+
         self.create_widgets()
 
     def create_widgets(self):
         # Enter the date.
         # Enter your question.
-        # Throw dice button.
-        # Display for the chart.
-        pass
-
-    def draw_chart(self):
-        my_shield = Shield.quick_cast()
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
-    
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "hellow Word\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        # Display the mother figures. If not fully thrown, show the blank page or the partial group of figures.
+        self.top_frame = tk.Frame(self)
+        self.top_frame.grid(row = 0)
+        self.figure_slots = []
+        self.figure_strings = []
+        for m in range(Shield.num_mothers):
+            cur_stringvar = tk.StringVar(self)
+            cur_stringvar.set(self.blank_lines)
+            self.figure_strings.append(cur_stringvar)
+            cur_label = tk.Label(self.top_frame, textvariable=cur_stringvar, font=self.default_font, bg = self.bgcolors[m])
+            self.figure_slots.append(cur_label)
+            cur_label.grid(row=0, column=Shield.num_mothers - m)
+        # The dice button.
+        self.one_throw_button = tk.Button(self, text = 'Throw the dice.', command = self.throw_dice)
+        self.one_throw_button.grid(row = 1)
+        # The chart button. Become enabled when all four Figure objects have been initiated.
+        self.chart_button = tk.Button(self, text = 'Create the chart.', command = self.create_chart, state = tk.DISABLED)
+        self.chart_button.grid(row = 2)
         
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                             command=root.destroy)
-        self.quit.pack(side="bottom")
-    
-    def say_hi(self):
-        print("hi there, everyone!")
+        self.start_over_button = tk.Button(self, text = 'Start over.', command = self.start_over, state = tk.DISABLED)
+        self.start_over_button.grid(row = 3)
 
+        self.chartvar = tk.StringVar()
+        self.chart = tk.Label(self, anchor=tk.W, textvariable=self.chartvar, font = self.default_font, state=tk.DISABLED)
 
-class PhoneApp(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.grid()
-        self.create_widgets()
-        print(self.grid_size())
-    
-    def create_widgets(self):
-        dial_keys = []
-        dial_keys.append([1,2,3])
-        dial_keys.append([4,5,6])
-        dial_keys.append([7,8,9])
-        dial_keys.append(["*",0,"#"])
-        self.buttons = {}
-        cur_column = 0
-        cur_row = 0
-        for row in dial_keys:
-            for k in row:
-                ks = str(k)
-                self.buttons[ks] = tk.Button(self, text=ks,
-                                           command=self.print_val(ks))
-                self.buttons[ks].grid(row=cur_row, column=cur_column)
-                cur_column += 1
-            cur_column = 0
-            cur_row += 1
+    def throw_dice(self):
+        fig = Figure.quick_throw()
+        cur_throw = self.cur_throw
+        self.figures[cur_throw] = fig
+        self.figure_slots[cur_throw].config(bg=self.bgcolors[cur_throw])
+        centered_str = center_lines(str(fig), len(Figure.longest_name))
+        self.figure_strings[cur_throw].set(centered_str)
+        self.cur_throw = (self.cur_throw + 1) % Shield.num_mothers
+        if all(self.figures):
+            self.chart_button.config(state = tk.NORMAL)
+        if any(self.figures):
+            self.start_over_button.config(state = tk.NORMAL)
 
-    def print_val(self, val):
-        return lambda: print(val)
-            
-root = tk.Tk()
-app_choice = {'ph':PhoneApp, 'app':Application}
-app = app_choice['ph'](master=root)
-root.mainloop()
+    def create_chart(self):
+        my_shield = Shield(*self.figures)
+        self.chartvar.set(my_shield.text_art())
+        self.chart.config(state=tk.NORMAL)
+        self.chart.grid(row = 4)
+
+    def start_over(self):
+        self.cur_throw = 0
+        for i in range(len(self.figures)):
+            self.figures[i] = None
+        for s in self.figure_strings:
+            s.set(self.blank_lines)
+        self.chart_button.config(state = tk.DISABLED)
+        self.chart.grid_forget()
+        self.start_over_button.config(state = tk.DISABLED)
+ 
+def run():
+    root = tk.Tk()
+    app = GeomancyApplication(master=root)
+    root.mainloop()
 
---
+++ run.py
@@ -0,0 +1,3 @@
+import geomancy.gui
+
+geomancy.gui.run()
